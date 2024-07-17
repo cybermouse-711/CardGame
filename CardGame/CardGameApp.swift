3e624f187a6dccab0f0ca465a24eb2807077812d
Liza <desseobscurite@gmail.com>
2024-07-17 14:20:50 +0400

    Initial Commit

---
+++ CardGame/CardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameApp.swift
+//  CardGame
+//
+//  Created by Elizaveta Medvedeva on 17.07.24.
+//
+
+import SwiftUI
+
+@main
+struct CardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
