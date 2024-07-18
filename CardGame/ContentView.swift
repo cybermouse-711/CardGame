2fff72729cd07c82564c6e40159bcb620d82e19d
Liza <desseobscurite@gmail.com>
2024-07-18 19:07:40 +0400

    Deleted element Button/Text and added element CardView

--- CardGame/ContentView.swift
+++ CardGame/ContentView.swift
@@ -8,17 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["🐼", "🐻", "🐻‍❄️", "🐨", "🐶", "🐹", "🐭", "🐰", "🐱"]
+    @State var emojiCount = 4
+    
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 20)
-                .stroke()
-            Text("Hello, world!")
+        VStack {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 75))]) {
+                ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                    CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
+                }
+            }
+            .foregroundStyle(.gray)
+            
+            Spacer()
+            HStack {
+                Button{
+                    if emojiCount > 1 {
+                        emojiCount -= 1
+                    }
+                }
+            label: { Image(systemName: "minus.circle") }
+                
+                Spacer()
+                Text("Change").foregroundStyle(.gray)
+                Spacer()
+                
+                Button {
+                    if emojiCount < emojis.count {
+                        emojiCount += 1
+                    }
+                }
+            label: { Image(systemName: "plus.circle") }
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
         }
-        .foregroundStyle(Color.red)
         .padding()
     }
 }
 
 #Preview {
     ContentView()
+        .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+}
+
+#Preview {
+    ContentView()
+        .preferredColorScheme(.light)
 }
