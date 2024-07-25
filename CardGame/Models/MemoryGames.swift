20f1ce84c4c45e25a486fbc68243fa6d9420c311
Liza <desseobscurite@gmail.com>
2024-07-25 18:21:50 +0400

    Changed func choose in MemoryGame

--- CardGame/Models/MemoryGames.swift
+++ CardGame/Models/MemoryGames.swift
@@ -20,8 +20,18 @@ struct MemoryGame<CardContent> {
         }
     }
     
-    func choose(_ card: Card) {
-        
+    mutating func choose(_ card: Card) {
+        let chosenIndex = index(of: card)
+        cards[chosenIndex].isFaceUp.toggle()
+    }
+    
+    private func index(of card: Card) -> Int {
+        for index in 0..<cards.count {
+            if cards[index].id == card.id {
+                return index
+            }
+        }
+        return 0
     }
     
     struct Card: Identifiable {
