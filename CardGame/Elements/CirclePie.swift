3c0de4b731e1ff220e504a7d15083371918ce2da
Liza <desseobscurite@gmail.com>
2024-07-29 20:10:57 +0400

    Created struct CirclePie for drawing Circle

---
+++ CardGame/Elements/CirclePie.swift
@@ -0,0 +1,38 @@
+//
+//  CirclePie.swift
+//  CardGame
+//
+//  Created by Elizaveta Medvedeva on 29.07.24.
+//
+
+import SwiftUI
+
+struct CirclePie: Shape {
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise = false
+    
+    func path(in rect: CGRect) -> Path {
+        
+        let center = CGPoint(x: rect.midX, y: rect.midY)
+        let radius = min(rect.width, rect.height) / 2
+        let start = CGPoint(
+            x: center.x + radius * CGFloat(cos(startAngle.radians)),
+            y: center.y + radius * CGFloat(sin(startAngle.radians))
+        )
+        
+        var p = Path()
+        p.move(to: center)
+        p.addLine(to: start)
+        p.addArc(
+            center: center,
+            radius: radius,
+            startAngle: startAngle,
+            endAngle: endAngle,
+            clockwise: !clockwise
+        )
+        p.addLine(to: center)
+        
+        return p
+    }
+}
